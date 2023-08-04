5243239ee971a8e654509b85d3cb1de996e7a607
Flynn Duniho <flynn@harveyosity.org>
2023-08-03 17:01:38 -0700

    Make the search display a notification with the title, artists, and album art

--- search-spotify.py
+++ search-spotify.py
@@ -1,6 +1,52 @@
+import io
+import os
+from notifypy import Notify
 import sys
+import requests
+import bs4
+from PIL import Image
+from datetime import datetime
+
 import spotifyapi
 
+iconpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{hash(datetime.now())}.png")
 spotify = spotifyapi.Session()
+track = spotify.search(sys.argv[1])["tracks"]["items"][0]
 
-print(spotify.search(sys.argv[1])["tracks"]["items"][0]["external_urls"]["spotify"])
+desc = track["name"] + " - " + ", ".join([s["name"] for s in track["artists"]])
+trackurl = track["external_urls"]["spotify"]
+
+page = requests.get(trackurl).text
+parsed = bs4.BeautifulSoup(page, features="lxml")
+
+img_element = parsed.find("img", attrs={ "alt":track["name"] })
+if not img_element:
+    img_element = parsed.find("img", attrs={ "alt":"" })
+
+imgurl = ""
+if isinstance(img_element, bs4.Tag):
+    if "srcset" in img_element:
+        imgurl = str(img_element["srcset"]).split(", ")[-1].split(" ")[0]
+    else:
+        imgurl = str(img_element["src"])
+
+has_icon = False
+if imgurl != "":
+    has_icon = True
+    imgreq = requests.get(imgurl)
+
+    img = Image.open(io.BytesIO(imgreq.content))
+    img.save(iconpath)
+
+notif = Notify()
+
+notif.title = "Search Result"
+notif.message = desc
+if has_icon:
+    notif.icon = iconpath
+
+notif.send()
+
+print(trackurl)
+
+os.remove(iconpath)
--- search-tidal.py
+++ search-tidal.py
@@ -1,9 +1,35 @@
+import io
+import os
+from notifypy import Notify
 import sys
-import tidallogin
+import requests
+import bs4
+from PIL import Image
+from datetime import datetime
 import tidalapi
 
+import tidallogin
+
+iconpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{hash(datetime.now())}.png")
 tidal = tidallogin.login()
 
-result = tidal.search(sys.argv[1], models= [ tidalapi.Track ], limit= 1)["tracks"][0]
+track = tidal.search(sys.argv[1], models= [ tidalapi.Track ], limit= 1)["tracks"][0]
+desc = f"{track.full_name} - {', '.join([a.name for a in track.artists])}"
 
-print("https://listen.tidal.com/track/" + str(result.id))
+imgurl = track.album.image(320)
+imgreq = requests.get(imgurl)
+
+img = Image.open(io.BytesIO(imgreq.content))
+img.save(iconpath)
+
+notif = Notify()
+
+notif.title = "Search Result"
+notif.message = desc
+notif.icon = iconpath
+
+notif.send()
+
+print("https://listen.tidal.com/track/" + str(track.id))
+
+os.remove(iconpath)
